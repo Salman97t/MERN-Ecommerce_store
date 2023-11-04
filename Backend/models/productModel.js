b46600ec0981dd2626d63ff3a3e9d5c6e43aaece
Salman97t <s.tariq1997@gmail.com>
2023-11-03 21:01:06 -0700

    productschema created

---
+++ Backend/models/productModel.js
@@ -0,0 +1,69 @@
+import mongoose from "mongoose";
+
+
+const productSchema= new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,"Please Enter Name for product"],
+        trim:true
+    },
+    descrption:{
+        type:String,
+        required:[true,"Please Enter Description for product"],
+    },
+    price:{
+        type:Number,
+        required:true,
+        maxLength: [4, "Price of product should not exceed 4 digits"]
+    },
+    rating:{
+        type:Number,
+        default:0,
+    },
+    images:[
+      {  public_id:{
+            type:String,
+            required:true,
+        },
+        url:{
+            type:String,
+            required:true,
+        }
+      }
+    ],
+    category:{
+        type:String,
+        required:[true, "Please enter category of product"]
+    },
+    stock:{
+        type:String,
+        required:[true, "Please enter porduct stock"],
+        maxLength:[50,"Stock cannot exceed 50 items"],
+        default:1
+    },
+    numberOfReviews:{
+        type:Number,
+        default:0
+    },
+    reviews:[
+        {
+            name:{
+                type:String,
+                required:true,
+            },
+            rating:{
+                type:Number,
+                required:true
+            },
+            comment:{
+                type:String,
+                required:true
+            }
+        }
+    ],
+    createdAt:{
+        type:Date,
+        default:Date.now
+    }
+})
+export default mongoose.model("product",productSchema);
