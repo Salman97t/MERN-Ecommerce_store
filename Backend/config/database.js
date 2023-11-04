93359023f4cabc5e04e91469137e26111637ca24
Salman97t <s.tariq1997@gmail.com>
2023-11-03 20:34:26 -0700

    Database Connected

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -1,2 +1,10 @@
 import mongoose from "mongoose";
-mongoose.connect()
+
+const connectDB =()=>{
+    mongoose.connect(process.env.Mongo_URI,{useNewUrlParser:true,useUnifiedTopology:true}).then((data)=>{
+        console.log(`MongoDB connected with server ${data.connection.host}. `);
+    }).catch((err)=>{
+        console.log(err);
+    })
+}
+export default connectDB;
--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,12 @@
 import app from './app.js';
 import dotenv from 'dotenv';
+import connectDB from './config/database.js';
 
 dotenv.config({path:"backend/config/config.env"});
+
+//connecting to database
+connectDB();
+
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server is running at https://localhost: ${process.env.PORT} `);
