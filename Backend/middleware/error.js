4aa42f00e164f1af399093b10ae7f27baa906efb
Salman97t <s.tariq1997@gmail.com>
2023-11-05 13:01:04 +0500

    MongoDb Id error + all error complete

--- Backend/middleware/error.js
+++ Backend/middleware/error.js
@@ -5,6 +5,12 @@ import errorhandler from "../utils/errorhander.js";
     const errorMiddleware = (err,req,res,next)=>{
             err.statusCode = err.statusCode || 500;
             err.message = err.message || "Internal Server Error";
+
+        // wrong MongoDB Id error
+        if(err.name === "CastError"){
+            const message = `Resource not found. Invalid: ${err.path}`;
+            err = new errorhandler(message,400)
+        }
         res.status(err.statusCode).json({
             success:false,
             message : err.message,
